c306935502de4a0646d7846da4ecfba1d5c5af3f
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-09 22:13:54 +0200

    Js <=> created a puppeteer browser instance in try-catch

--- scraper/movieDataScraper.js
+++ scraper/movieDataScraper.js
@@ -2,3 +2,15 @@ import puppeteer from "puppeteer";
 import filmsUrls from "./filmsUrls.js";
 import fs from "fs/promises";
 
+const getMovieData = async (url,retries = 3)=>{
+    let browser;
+    try{
+        browser = await puppeteer.launch({
+            headless:false,
+            defaultViewport:null,
+        });
+
+    }catch(error){
+        
+    }
+}
