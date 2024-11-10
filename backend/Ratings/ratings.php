3d5c21c413a2c7daa15fd47cc0e020dbc4b89641
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-10 19:51:04 +0200

    php <=> API for updating ratings

--- backend/Ratings/ratings.php
+++ backend/Ratings/ratings.php
@@ -14,3 +14,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Retrieve user_id and movie_id from POST data
+    $user_id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
+    $movie_id = isset($_POST['movie_id']) ? $_POST['movie_id'] : null;
+    
+    $sql = "SELECT * FROM ratings";
+    $conditions = [];
+
+    if ($user_id) $conditions[] = "user_id = '$user_id'";
+    if ($movie_id) $conditions[] = "movie_id = '$movie_id'";
+
+    if (count($conditions) > 0) {
+        $sql .= " WHERE " . implode(" AND ", $conditions);
+    }
+
+    $result = $connection->query($sql);
+    $ratings = [];
+    while ($row = $result->fetch_assoc()) {
+        $ratings[] = $row;
+    }
+    echo json_encode($ratings);
+}
