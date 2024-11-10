93b8fb20f907262bff0212442ebf696bb6f0aef0
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-10 20:59:02 +0200

    php <=> implemented a for loop to iterate through the data array and insert each into db

--- backend/seeding.php
+++ backend/seeding.php
@@ -1,11 +1,30 @@
 <?php
 
 include "connection.php";
-
+$count=0;
 $jsonData1 = file_get_contents("../scraper/movieDataCollection1.json");
 $jsonData2 = file_get_contents("../scraper/movieDataCollection2.json");
 
 $moviesArray1 = json_decode($jsonData1,true) ;
 $moviesArray2 = json_decode($jsonData2,true) ;
 
- echo gettype($moviesArray1);
+if ($moviesArray1==null || $moviesArray2==null){
+    echo "Error decoding JSON: ";
+    exit;
+}
+foreach ($moviesArray1 as $movie){
+    $title = $connection->real_escape_string($movie["title"]);
+    $duration = (int)$movie["duration"];
+    $genre = $connection->real_escape_string($movie["genre"]);
+    $imageUrl = $connection->real_escape_string($movie["imageUrl"]);
+    $releaseDate = $connection->real_escape_string($movie["releaseDate"]);
+    $description = $connection->real_escape_string($movie["description"]);
+
+    $query = $connection->prepare("INSERT INTO movies (title, duration, genre, imageUrl, releaseDate, description) 
+            VALUES (?, ?, ?, ?, ?, ?)");
+    $query->bind_param("sissss",$title,$duration,$genre,$imageUrl,$releaseDate,$description);
+    $query->execute();
+    if($query->affected_rows>0){
+        $count++;
+    }
+}
