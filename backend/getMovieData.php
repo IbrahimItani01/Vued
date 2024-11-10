cda857d917e85e1072e6f22c1393e4e864c43061
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-10 19:28:12 +0200

    php <=> implemented an api to return movie info via POST method based on certain id

---
+++ backend/getMovieData.php
@@ -0,0 +1,25 @@
+<?php
+include "connection.php";
+$targetId = $_POST["movieId"];
+
+$query = $connection->prepare("SELECT * from movies where id=?");
+$query->bind_param("i", $targetId);
+$query->execute();
+$result = $query->get_result();
+if($query->num_rows > 0) {
+    http_response_code(200);
+    $response =[
+        "status"=>"success",
+        "message"=>"records fetched",
+        "array"=>$result->fetch_assoc(),
+    ];
+}
+else {
+    http_response_code(404);
+    $response =[
+        "status"=>"error",
+        "message"=>"empty record",
+        "array"=>[],
+    ];
+}
+echo json_encode($response);
