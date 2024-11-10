b8eaa132bb3825a1813ce885afc53ea2a1c1eb6c
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-10 20:38:25 +0200

    php <=> API for creating bookmarks

---
+++ backend/Bookmarks/bookmarks.php
@@ -0,0 +1,18 @@
+<?php
+include ('../connection.php');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user_id = $_POST['user_id'];
+    $movie_id = $_POST['movie_id'];
+
+    $stmt = $connection->prepare("INSERT INTO bookmarks (user_id, movie_id) VALUES (?, ?)");
+
+    $stmt->bind_param("ii", $user_id, $movie_id);
+
+    if ($stmt->execute()) {
+        echo json_encode(["message" => "Booksmark added successfully"]);
+    } else {
+        echo json_encode(["error" => "Error: " . $stmt->error]);
+    }
+
+    $stmt->close();
+}
