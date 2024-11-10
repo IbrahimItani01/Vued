0001b33c21f7990f43e7cb3d8e4cb700fc545b20
unknown <ryanchehab20@gmail.com>
2024-11-10 21:50:14 +0200

    php <=> added duplicate prevention

--- backend/signup.php
+++ backend/signup.php
@@ -8,6 +8,27 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
     $password= $_POST['password'];
     $fav_genre = $_POST['fav_genre'];
 }
+
+$sql2= 'SELECT email FROM users where email = ?';
+
+$stmt2 = $connection->prepare($sql2);
+
+$stmt2->bind_param("s",$email);
+
+$stmt2->execute();
+
+
+if($stmt->num_rows > 0){
+    $response = [];
+    $response['status'] = 'failed';
+    $response['message'] = 'User already registered';
+    echo json_encode($response);
+
+    // take the user to login page
+    // header();
+}
+else{
+    // adding the user 
 $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
 echo $hashed_pass;
 $sql = "INSERT INTO users(email,password,name,favorite_genre,banned,user_type) VALUES(?,?,?,?,?,?)";
@@ -25,8 +46,11 @@ if ($stmt->execute()){
     $response['status'] = 'success';
     $response['message'] = 'User successfully registered!';
     echo json_encode($response);
+
+    // take the user to login page
     // header();
 }else{
     $response['status'] = 'error';
     $response['message'] = 'error preparing statement' . $stmt->error;
+}
 }
