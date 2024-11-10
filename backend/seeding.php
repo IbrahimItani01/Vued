1d130da660241322695e5e1cb13d2a051bb7091e
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-10 20:45:17 +0200

    php <=> decoded json data into an associative array

--- backend/seeding.php
+++ backend/seeding.php
@@ -3,6 +3,9 @@
 include "connection.php";
 
 $jsonData1 = file_get_contents("../scraper/movieDataCollection1.json");
+$jsonData2 = file_get_contents("../scraper/movieDataCollection2.json");
 
+$moviesArray1 = json_decode($jsonData1,true) ;
+$moviesArray2 = json_decode($jsonData2,true) ;
 
-echo $jsonData1;
+ echo gettype($moviesArray1);
