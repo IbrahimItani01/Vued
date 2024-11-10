203e329cec143df1590e0b4dd3626caacada834b
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-10 21:02:57 +0200

    php<=>implemented response for resolved and rejected statuses

--- backend/seeding.php
+++ backend/seeding.php
@@ -28,3 +28,34 @@ foreach ($moviesArray1 as $movie){
         $count++;
     }
 }
+foreach ($moviesArray2 as $movie){
+    $title = $connection->real_escape_string($movie["title"]);
+    $duration = (int)$movie["duration"];
+    $genre = $connection->real_escape_string($movie["genre"]);
+    $imageUrl = $connection->real_escape_string($movie["imageUrl"]);
+    $releaseDate = $connection->real_escape_string($movie["releaseDate"]);
+    $description = $connection->real_escape_string($movie["description"]);
+
+    $query = $connection->prepare("INSERT INTO movies (title, duration, genre, imageUrl, releaseDate, description) 
+            VALUES (?, ?, ?, ?, ?, ?)");
+    $query->bind_param("sissss",$title,$duration,$genre,$imageUrl,$releaseDate,$description);
+    $query->execute();
+    if($query->affected_rows>0){
+        $count++;
+    }
+}
+if($count>0){
+    http_response_code(200);
+    $response= [
+        "status"=>"success",
+        "message"=>"$count movies added to db",
+    ];
+}
+else{
+    http_response_code(404);
+    $response= [
+        "status"=>"success",
+        "message"=>"movies couldn't be added to db",
+    ];
+}
+echo json_encode($response);
