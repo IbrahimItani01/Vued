dc7ed06d154121add82012bf66d577c84c15ef7e
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-12 01:02:01 +0200

    php <=> added http response and error handling to userData

--- backend/UserData/userdata.php
+++ backend/UserData/userdata.php
@@ -10,7 +10,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     while ($row = $result->fetch_assoc()) {
         $users[] = $row;
     }
+    if(count($users) > 0) {
+        http_response_code(200);
         echo json_encode($users);
-}
+    }
+    else{
+        http_response_code(404);
+        echo "no users in db";
+    }
 
-?>
+}
