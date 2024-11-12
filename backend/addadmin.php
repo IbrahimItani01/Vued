6542226219cdcd3ccc1167cd70465e587c4fdfa2
unknown <ryanchehab20@gmail.com>
2024-11-12 08:57:09 +0200

    php <=> retrieved data from admin

--- backend/addadmin.php
+++ backend/addadmin.php
@@ -1,2 +1,10 @@
 <?php
 session_start();
+
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password= $_POST['password'];
+}
+
+$fav_genre = "none";
