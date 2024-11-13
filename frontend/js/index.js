5d5f330f68ec375b9ce69f695d466fee263e1391
unknown <ryanchehab20@gmail.com>
2024-11-13 03:11:15 +0200

    js <=> selected login elements

---
+++ frontend/js/index.js
@@ -0,0 +1,5 @@
+// login
+const login_email = document.getElementById('login-email');
+const login_pass = document.getElementById('login-pass');
+const login_btn = document.getElementById('login-btn');
+
