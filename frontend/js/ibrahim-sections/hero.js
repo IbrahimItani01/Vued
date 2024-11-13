275ce891a950d3ef57c01caaa341970b9524c6f6
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-13 19:47:37 +0200

    js<=> fetched elements of hero section

---
+++ frontend/js/ibrahim-sections/hero.js
@@ -0,0 +1,6 @@
+const movieImage = document.getElementById("hero-movie-img")
+const movieTitle = document.getElementById("hero-movie-title");
+const movieYear = document.getElementById("hero-movie-year");
+const movieGenre = document.getElementById("hero-movie-genre");
+const movieDuration = document.getElementById("hero-movie-duration");
+
