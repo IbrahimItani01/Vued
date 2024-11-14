2532ac62216530fefd21f6e8647663e889046087
RyanChehab <ryanchehab20@gmail.com>
2024-11-14 08:35:48 +0200

    php <=> stored result after executing the query

--- backend/getusers.php
+++ backend/getusers.php
@@ -3,6 +3,14 @@ include 'connection.php';
 
 $sql = 'SELECT * From users where banned = ?';
 
+$banned = "0";
+
 $stmt = $connection->prepare($sql);
 
+$stmt->bind_param("s",$banned);
 
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+while($user = $stmt->fetch_assoc())
