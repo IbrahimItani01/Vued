6154caa5c13c5d189c88c7961248679f93bb598b
RyanChehab <ryanchehab20@gmail.com>
2024-11-14 07:34:52 +0200

    php <=> fixed paths

--- backend/authentication/login.php
+++ backend/authentication/login.php
@@ -4,7 +4,7 @@ session_start();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-include 'connection.php';
+include('../connection.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $email = $_POST['login_email'];
