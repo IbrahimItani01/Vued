0e9530da1ce7b401817d5a29aaeeb08024e90412
RyanChehab <ryanchehab20@gmail.com>
2024-11-14 07:03:41 +0200

    js <=> sent the data to add admin api correctly

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,3 +1,27 @@
 const username = document.getElementById('admin-name')
 const email = document.getElementById('admin-email')
 const password = document.getElementById('admin-password')
+const add_btn = document.getElementById('add-admin-btn')
+
+add_btn.addEventListener('click',async(e)=>{
+    e.preventDefault();
+
+    if(!username.value || !email.value || !password.value){
+        alert('Please fill in all fields.')
+        return
+    }
+
+    const data = new FormData();
+    data.append("name",username)
+    data.append("email",email)
+    data.append("password",password)
+
+    try{
+        const result = await fetch('http://localhost/vued/backend/addadmin.php',{
+            method: 'POST',
+            body: data
+        })
+        const response = await result.json()
+        console.log(response)
+    }catch(error){}
+})
